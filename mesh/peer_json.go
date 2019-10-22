de3aae9f2114b6a57daca9552e90ef6a1aa1cf24
Simone Margaritelli <evilsocket@gmail.com>
2019-10-22 19:43:17 +0200

    misc: small fix or general refactoring i did not bother commenting

--- mesh/peer_json.go
+++ mesh/peer_json.go
@@ -29,7 +29,7 @@ func (peer *Peer) json() *jsonPeer {
 	// see https://www.patreon.com/posts/bonding-equation-30954153
 	var bond float64
 	t := float64(time.Since(peer.MetAt).Seconds() + 1e-50) // avoid division by 0
-	e := float64(peer.Encounters)
+	e := float64(peer.Encounters / 5)
 	bond = e / t
 
 	log.Debug("bond for %s: secs_since_met=%f encounters=%d bond=%f",
