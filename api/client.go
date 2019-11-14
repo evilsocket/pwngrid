418d0ca2a523dcc53c0ddca183fe6e5d0c14c0cf
Simone Margaritelli <evilsocket@gmail.com>
2019-11-14 13:31:56 +0100

    fix: don't enroll unit when mesh data is updated (ref https://github.com/evilsocket/pwnagotchi/issues/582)

--- api/client.go
+++ api/client.go
@@ -176,12 +176,6 @@ func (c *Client) SetData(newData map[string]interface{}) map[string]interface{} 
 		}
 	}
 
-	if time.Since(c.tokenAt) >= models.TokenTTL {
-		if err := c.enroll(); err != nil {
-			log.Error("error while refreshing token: %v", err)
-		}
-	}
-
 	return c.data
 }
 
